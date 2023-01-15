9ac6838c538d1cde40b8db9508c4f32d535aad17
FkkfRf <Fkkf.Rf@gmail.com>
2023-01-15 18:40:23 +0300

    login

--- src/test/java/spec/ResponseSpecs.java
+++ src/test/java/spec/ResponseSpecs.java
@@ -4,7 +4,6 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.ResponseSpecification;
 
 import static io.restassured.filter.log.LogDetail.*;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class ResponseSpecs {
 
@@ -18,7 +17,7 @@ public class ResponseSpecs {
             .expectStatusCode(500)
             .build();
 
-    public static ResponseSpecification createSuccessResponseSpec = new ResponseSpecBuilder()
+   /* public static ResponseSpecification createSuccessResponseSpec = new ResponseSpecBuilder()
             .log(STATUS)
             .log(BODY)
             .expectStatusCode(201)
@@ -28,15 +27,6 @@ public class ResponseSpecs {
             .expectBody("createdAt", notNullValue())
             .build();
 
-    public static ResponseSpecification createUnSuccessResponseSpec = new ResponseSpecBuilder()
-            .log(ALL)
-            //.log(BODY)
-            .expectStatusCode(400)
-            .build();
+    */
 
-    public static ResponseSpecification deleteResponseSpec = new ResponseSpecBuilder()
-            .log(STATUS)
-            .log(BODY)
-            .expectStatusCode(204)
-            .build();
 }
